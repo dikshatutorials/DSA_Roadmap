ab1c9f12ff979bbeb882c8ff8e896669f88ba66b
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-20 23:11:40 +0530

    commit

---
+++ Basics/01_user_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter first number (a): ";
+    cin>>a;
+    cout<<"Enter second number (b): ";
+    cin>>b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b<<endl;
+
+    return 0;
+}
