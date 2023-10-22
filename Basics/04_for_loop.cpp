889a49ba591b0b4bbb4e104b55e04b2d41a35c0f
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-22 21:38:54 +0530

    commit

---
+++ Basics/04_for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for-loops in C++
+    int i;
+    for (i = 0; i < 100; i++)
+    {
+        cout << "Hello World!" << endl;
+    }
+
+    return 0;
+}
